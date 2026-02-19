67015ed1c290f5a1d37f57dc070fa27b6d58d5d9
Chonang Rai <chonang.rai@36t-hub.com>
2026-02-19 17:22:43 +0000

    Working on displaying formula on summary table

---
+++ app/static/assets/LineChart-DnFWQS23.js
@@ -0,0 +1,1 @@
+import{a2 as a,X as i,Y as s,a3 as r}from"./generateCategoricalChart-8XRKW6zQ.js";import{L as x}from"./BarChart-mpOYfoJR.js";var t=a({chartName:"LineChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:s}],formatAxisMap:r});export{t as L};
